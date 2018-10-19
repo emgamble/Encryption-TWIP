bd39411cfa9a5e4b6ff713a0f808b6cc14f9ea8e
emgamble <eg913@pvsd.org>
2018-10-18 22:57:40 -0400

    It doesn't work, all it does is output "ahhhhh" (seriously it does this) which I can only assume is the sound of it screaming because I am so bad at this. I don't even know anymore

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,8 +9,34 @@ public class Main {
        Scanner wordReader = new  Scanner(System.in);
        System.out.println("Enter the word you want encrypted here: ");
        String a = wordReader.nextLine();
-       int cesar = (int) (Math.random() * 26);
-       System.out.print(cesar);
+       int caesar = (int) (Math.random() * 10);
+       //System.out.print(caesar);
+        char alphabet [] = {'a','b', 'c', 'd','e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+                'p', 'q', 'r','s', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+        a.toLowerCase();
+        char word [] = a.toCharArray();
+
+        System.out.print( alphabet [0]);
+        for(int j = 0; j < a.length(); j++) {
+
+            int k = 0;
+            int p = 0;
+
+            if (word[k] == alphabet[p]) {
+                if ((k + caesar) > alphabet.length) {
+                    int l = (alphabet.length - k);
+                    System.out.print(alphabet[l]);
+                }
+                else {
+                    System.out.print(alphabet[k + caesar]);
+                }
+            }
+            p++;
+            k++;
+        }
+
+
+
 
 
     }
